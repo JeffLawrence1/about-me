b270e1c83ad5722ea5ac006045e401bbedd092be
Jeff Lawrence <jefflaw13@hotmail.com>
2019-03-18 16:23:23 -0700

    fixed indentation problems

--- js/app.js
+++ js/app.js
@@ -8,41 +8,41 @@ function startGame(){
     var color = prompt('Is my favorite color yellow?');
     console.log('the user answered ' + color);
     if(color.toUpperCase() === 'N' || color.toUpperCase() === 'NO'){
-        alert('Correct!');
+      alert('Correct!');
     }else{
-        alert('WRONG!!!');
+      alert('WRONG!!!');
     }
 
     var trees = prompt('Do I like trees?');
     console.log('the user answered ' + trees);
     if(trees.toUpperCase() === 'Y' || trees.toUpperCase() === 'YES'){
-        alert('Correct!');
+      alert('Correct!');
     }else{
-        alert('WRONG!!!');
+      alert('WRONG!!!');
     }
 
     var car = prompt('Do I drive a go cart?');
     console.log('the user answered ' + car);
     if(car.toUpperCase() === 'N' || car.toUpperCase() === 'NO'){
-        alert('Correct!');
+      alert('Correct!');
     }else{
-        alert('WRONG!!!');
+      alert('WRONG!!!');
     }
 
     var pet = prompt('Do I have a dog?');
     console.log('the user answered ' + pet);
     if(pet.toUpperCase() === 'N' || pet.toUpperCase() === 'NO'){
-        alert('Correct!');
+      alert('Correct!');
     }else{
-        alert('WRONG!!!');
+      alert('WRONG!!!');
     }
 
     var age = prompt('Am I over 21?');
     console.log('the user answered ' + age);
     if(age.toUpperCase() === 'Y' || age.toUpperCase() === 'YES'){
-        alert('Correct!');
+      alert('Correct!');
     }else{
-        alert('WRONG!!!');
+      alert('WRONG!!!');
     }
 
   }else{
